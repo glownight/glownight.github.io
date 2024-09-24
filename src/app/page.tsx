77382ac4977394c45eadf86acb2d400362ea6596
glownight <885204655@qq.com>
2024-09-25 01:16:08 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>晚安😴</p>
-        <p>一切都会好好的。</p>
         <Button className="bg-[#20985e]" type="primary">
-          点我加油～
+          早安❤
         </Button>
         <Divider className="bg-[#1f8e94]" dashed />
         <p className="bg-[#186f74]">“最最喜欢你，绿子。”</p>
