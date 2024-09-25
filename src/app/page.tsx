a8f7bc05f3499e9ad824dcb3027bad0248f8dd20
glownight <885204655@qq.com>
2024-09-26 00:05:03 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="/about">
+        <Link href="/">
           <Button className="bg-[#20985e]" type="primary">
             亲爱的，早安❤
           </Button>
