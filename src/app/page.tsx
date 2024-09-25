77f479a1f1008cd04171a5e0d45846c38b51acc4
glownight <885204655@qq.com>
2024-09-26 00:29:15 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,66 @@
-import { Button, Divider } from "antd";
+import { Button, Divider, Timeline } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
 import "./globals.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="/">
-          <Button className="bg-[#20985e]" type="primary">
-            亲爱的，早安❤
-          </Button>
-        </Link>
+      <div className="flex flex-col row-start-2 items-center sm:items-start">
+        <Timeline
+          items={[
+            {
+              color: "green",
+              children: "今天要开心",
+            },
+            {
+              color: "red",
+              children: (
+                <>
+                  <p>一切都会好好的</p>
+                </>
+              ),
+            },
+            {
+              children: (
+                <>
+                  <p>我们会暴富</p>
+                </>
+              ),
+            },
+            {
+              color: "orange",
+              children: (
+                <>
+                  <p>我们会一直很健康</p>
+                </>
+              ),
+            },
+            {
+              color: "#00CCFF",
+              dot: <SmileOutlined />,
+              children: <p>加油哦</p>,
+            },
+          ]}
+        />
+        <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <Link href="/">
+            <Button className="bg-[#20985e]" type="primary">
+              亲爱的，早安❤
+            </Button>
+          </Link>
 
-        <Divider className="bg-[#1f8e94]" dashed />
-        <p className="bg-[#186f74]">“最最喜欢你，绿子。”</p>
-        <p className="testp2">“什么程度？”</p>
-        <p>“像喜欢春天的熊一样。”</p>
-        <p>“春天的熊？”绿子再次仰起脸，“什么春天的熊？”</p>
-        <p>
-          “春天的原野里，你一个人正走着，对面走来一只可爱的小熊，浑身的毛活像天鹅绒，眼睛圆鼓鼓的。它这么对你说道：‘你好，小姐，和我一块儿打滚玩好么？’接着，你就和小熊抱在一起，顺着长满三叶草的山坡咕噜咕噜滚下去，整整玩了一天。你说棒不棒？”
-        </p>
-        <p>“太棒了。” “我就这么喜欢你。”</p>
-      </main>
+          <Divider className="bg-[#1f8e94]" dashed />
+          <p className="bg-[#186f74]">“最最喜欢你，绿子。”</p>
+          <p className="testp2">“什么程度？”</p>
+          <p>“像喜欢春天的熊一样。”</p>
+          <p>“春天的熊？”绿子再次仰起脸，“什么春天的熊？”</p>
+          <p>
+            “春天的原野里，你一个人正走着，对面走来一只可爱的小熊，浑身的毛活像天鹅绒，眼睛圆鼓鼓的。它这么对你说道：‘你好，小姐，和我一块儿打滚玩好么？’接着，你就和小熊抱在一起，顺着长满三叶草的山坡咕噜咕噜滚下去，整整玩了一天。你说棒不棒？”
+          </p>
+          <p>“太棒了。” “我就这么喜欢你。”</p>
+        </div>
+      </div>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
